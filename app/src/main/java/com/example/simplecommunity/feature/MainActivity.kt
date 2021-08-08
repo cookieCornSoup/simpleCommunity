32523019857465aa09e95630121c88243512d5f5
cookieCornSoup <tonyjjh0513@naver.com>
2021-08-08 14:21:00 +0900

    피드용 카드뷰 추가

--- app/src/main/java/com/example/simplecommunity/feature/MainActivity.kt
+++ app/src/main/java/com/example/simplecommunity/feature/MainActivity.kt
@@ -26,5 +26,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inf
         }
 
 
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/simplecommunity/repository/FeedAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.simplecommunity.repository
+
+class FeedAdapter {
+
+
+    inner class FeedViewHolder()
+}
